8ff4a3ef93e0f21d1f306c1e4479e92647672dd9
nicollenaranjo <nicolle01@javerianacali.edu.co>
2023-03-20 11:16:21 -0500

    PSI-192 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,33 @@
-import { useState } from 'react';
-
+import React, { useState } from 'react';
 import { Button, Container, Stack } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import Login from './components/Login';
 
 function App() {
 
-  const [btnMsg, setBtnMsg] = useState('');
-
   return (
+    <>
     <Container className="p-3">
       <Container className="p-5 mb-4 bg-light rounded-3">
         <h1 className="header">React-Typescript + React-Bootstrap + Font-Awesome</h1>
-        
+        <p></p>
         <Stack direction='horizontal' gap={1} style={{minHeight: 24}} className='mb-2'>
-        {
-          (btnMsg !== '') &&
-            <>
-              <p className='m-0'>{btnMsg}</p>
-              <FontAwesomeIcon icon={faCoffee}/>
-            </>
-        }
+       
         </Stack>
 
         <Container className='ps-0'>
           <Stack direction='horizontal' gap={1}>
-            <Button onClick={() => {setBtnMsg('Button clicked!')}} variant='primary'>Click me</Button>
-            <Button onClick={() => {setBtnMsg('')}} variant='secondary'>Clear</Button>
+            <Button onClick={() => {('Button clicked!')}} variant='primary'>Click me</Button>
+            <Button onClick={() => {('')}} variant='secondary'>Clear</Button>
           </Stack>
         </Container>
       </Container>
     </Container>
+    
+    <Login/>
+
+    </>
   );
 }
 

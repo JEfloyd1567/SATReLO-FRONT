399dc3b13422a305501d51079ad44f412b672967
nicollenaranjo <nicolle01@javerianacali.edu.co>
2023-03-20 11:31:42 -0500

    Revert "Revert "Revert "PSI-192 completed"""
    This reverts commit 0a05040d0de8208b9c1a769bab04d4da318aa8ee.

--- src/components/IsNotLastButton.tsx
+++
@@ -1,9 +0,0 @@
-const IsNotLastButton = (pressedButtons:string,value:string) => {
-    if (pressedButtons[pressedButtons.length - 1] === value){
-        return false;
-    } else if (pressedButtons.includes(value)) {
-        return true;
-    }
-};
-
-export default IsNotLastButton;

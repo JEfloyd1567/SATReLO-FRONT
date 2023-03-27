e8125f409ca00e1037a0115abc516e2af50bd0d0
t0rRent <torrent@javerianacali.edu.co>
2023-03-26 20:35:55 -0500

    PSI-128 Therapist Modal added

--- src/components/HomeLoginButton/HomeLoginButton.tsx
+++ src/components/HomeLoginButton/HomeLoginButton.tsx
@@ -1,12 +1,19 @@
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import Therapist from "../TherapistModal/Therapist";
+import Button from 'react-bootstrap/Button';
 
 export const HomeLoginButton =()=>{
     const[clicked,setClicked] = useState(false)
     const hanndleButtonClick =()=>{
         setClicked(true)
     }
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return(
         <>
         <div className="position-absolute bottom-0 start-50 translate-middle-x homeLoginButton">
@@ -23,6 +30,8 @@ export const HomeLoginButton =()=>{
                     <div className="clickedBackgroundIcon rounded-circle d-inline col">
                         <FontAwesomeIcon icon={faArrowRight} style={{color: "white", width: "30px", height: "30px"}} />
                     </div>
+                    <Button variant="primary" onClick={handleShow}>testo</Button>
+                    <Therapist show={show} handleClose={handleClose}/>
                 </div>
             </div>
             </>

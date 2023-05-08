0cf4686bce881ed8e6ecf610344bd8f429cd844f
t0rRent <torrent@javerianacali.edu.co>
2023-05-07 19:29:22 -0500

    PSI-356 First Commit

---
+++ src/components/ActivitiesCreation/ActivitiesCreationContent.tsx
@@ -0,0 +1,11 @@
+import { FC, useState } from "react";
+
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+export const ActivitiesCreationContent = () => {
+    return(
+        /*<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 gy-4 d-flex align-items-center h-auto"></div>*/
+        <form></form>
+    )
+}

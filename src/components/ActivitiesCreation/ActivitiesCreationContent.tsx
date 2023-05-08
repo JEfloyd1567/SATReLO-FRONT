b7366f8f292dd95c8055b5dc31891319a9334923
t0rRent <torrent@javerianacali.edu.co>
2023-05-07 22:16:29 -0500

    PSI-356 Box Changes

--- src/components/ActivitiesCreation/ActivitiesCreationContent.tsx
+++ src/components/ActivitiesCreation/ActivitiesCreationContent.tsx
@@ -1,11 +1,33 @@
-import { FC, useState } from "react";
-
+import { Image } from 'react-bootstrap';
+import { Row, Col } from "react-bootstrap";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from 'react';
 
 export const ActivitiesCreationContent = () => {
+    const [objButtons, setObjButtons] = useState([
+        { className: "lunita", cssC: "css-lunita", image: "", identifier: '1', value: 0 },
+        { className: "arbolito", cssC: "css-arbolito", image: "", identifier: '2', value: 0 },
+        { className: "solicito", cssC: "css-solicito", image: "", identifier: '3', value: 0 },
+        { className: "gatito", cssC: "css-gatito", image: "", identifier: '4', value: 0 },
+        { className: "perrito", cssC: "css-perrito", image: "", identifier: '5', value: 0 },
+    ]);
+
+    const [pressedButtons, setPressedButtons] = useState(""); //combinación botones
+    let [iconCount, setIconPress] = useState(1); //combinación botones
     return(
-        /*<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 gy-4 d-flex align-items-center h-auto"></div>*/
-        <form></form>
+        <>
+            <div className="createActivities">
+                <div className='boxActivityPatient'></div>
+                <div className='boxActivity1'></div>
+                <div className='boxActivity2'></div>
+                <div className='boxActivity3'></div>
+                <div className='boxActivity4'></div>
+                <div className='boxActivity5'></div>
+                <div className='boxActivity6'></div>
+                <div className='boxActivity7'></div>
+                <button className='createActivityButton'></button>
+            </div>
+        </>
     )
 }
